2e9114dbf0b9f95b403f7731ff281c989c510eb2
koechronix <koechronix@yahoo.com>
2022-07-25 14:25:38 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/Home";
+import {Routes, Route} from "react-router-dom";
+import Product from "./components/Product";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+   <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={Home} />
+      <Route path="products" element={Products} />
+      <Route path="products/:id" element={Products} />
+      <Route path="cart" element={Cart} />
+    </Routes>
+   </div>
   );
 }
 
