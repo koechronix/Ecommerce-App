1bbcef360611cdd82fb2c1e0cb95cc050893f599
koechronix <koechronix@yahoo.com>
2022-07-25 14:46:34 -0400

    created another folder redux

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import handleCart from "./../redux/reducer/handleCart";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      Navbar
+      </div>
   )
 }
 
