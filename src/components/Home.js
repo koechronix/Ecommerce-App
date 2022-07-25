4fef39ed6c3f525d6ac710f61921652122085a75
koechronix <koechronix@yahoo.com>
2022-07-25 15:32:35 -0400

    worked on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from "react";
+import Products from "./Products";
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+     <div className="hero">
+        <div class="card bg-dark text-white border-0">
+          <img
+            src="./asset/img1.jpg"
+            class="card-img"
+            alt="background"
+            height="550px"
+          />
+          <div class="card-img-overlay d-flex justify-content-center flex-column">
+            <div className="container">
+              <h5 class="card-title display-3 fw-bolder mb-0">
+                New  Arrivals
+              </h5>
+              <p class="card-text lead fs-2">Check all trends in our shop</p>
+            </div>
+          </div>
+        </div>
+        <Products />
+      </div>
+
+    </>
   )
 }
 
