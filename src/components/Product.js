26e900ec927a6f651c62cca4734c680173cf933c
koechronix <koechronix@yahoo.com>
2022-07-25 22:12:02 -0400

    initialized fetch

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,6 +6,26 @@ import { useParams } from "react-router-dom";
 const Product = () => {
   const { id } = useParams;
   const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const addProduct = (product) =>{
+    dispatch(addCart(product));
+    // console.log(product, "product")
+  };
+
+  useEffect (() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+      setProduct(await res.json());
+      setLoading(false);
+    }
+
+       getProduct ();
+
+  }, [])
   return (
     <div>Product</div>
   )
