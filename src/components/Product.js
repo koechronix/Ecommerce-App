74a37ddef568f1a9a42f4f6dd8c9b6a793eecbe4
koechronix <koechronix@yahoo.com>
2022-07-25 21:55:02 -0400

    initialized usestate hook

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 const Product = () => {
   const { id } = useParams;
+  const [product, setProduct] = useState([]);
   return (
     <div>Product</div>
   )
