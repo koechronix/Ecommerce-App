55d03fcdb0dc441922d92d34dc96158d6e6a79f5
koechronix <koechronix@yahoo.com>
2022-07-25 22:16:38 -0400

    added useeffect

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 
@@ -26,6 +26,26 @@ const Product = () => {
        getProduct ();
 
   }, [])
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-md-6">
+          <Skeleton height={400} />
+        </div>
+        <div className="col-md-6" style={{ lineHeight: 2 }}>
+          <Skeleton height={50} width={300} />
+          <Skeleton height={75} />
+          <Skeleton height={25} width={150} />
+          <Skeleton height={50} />
+          <Skeleton height={150} />
+          <Skeleton height={50} width={100} />
+          <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
+        </div>
+      </>
+    );
+  };
+
+  
   return (
     <div>Product</div>
   )
