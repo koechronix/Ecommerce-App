7e05940136c1ea5ff398084823a81a479d05e920
koechronix <koechronix@yahoo.com>
2022-07-25 23:18:07 -0400

    used bootstrap

--- src/components/Products.js
+++ src/components/Products.js
@@ -42,9 +42,97 @@ const Products = () => {
       </>
     );
   };
-  return (
-    <div>Products</div>
-  )
-}
 
-export default Products
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((x) => x.category === cat);
+    setFilter(updatedList);
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          {/* <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            jewelries
+          </button> */}
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronics
+          </button>
+        </div>
+        {filter.map((product) => {
+          return (
+            <div className="col-md-3 mb-5" key={product.id}>
+              <div class="card h-100 text-center p-4 ">
+                <div className="d-flex justify-content-center align-item-center">
+                  <img
+                    src={product.image}
+                    class="card-img-top"
+                    alt={product.title}
+                    style={{ width: "100px", height: "100px" }}
+                  />
+                </div>
+
+                <div class="card-body">
+                  <h5 class="card-title mb-0">
+                    {product.title.substring(0, 12)}...
+                  </h5>
+                  <p class="card-text lead fw-bold">
+                    {" "}
+                    <i class="fa-solid fa-rupee-sign "></i> {product.price}
+                  </p>
+                  <Link to={`${product.id}`} className="btn btn-outline-dark">
+                    Buy Now
+                  </Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center ">
+              Latest Products
+            </h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Products;
