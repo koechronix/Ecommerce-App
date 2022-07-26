03f317697e3a81c02c102497b4fab207bbd59cf5
koechronix <koechronix@yahoo.com>
2022-07-25 22:54:19 -0400

    used useeffect

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import { Link } from "react-router-dom";
 
 const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+
+  let componentMount = true;
   return (
     <div>Products</div>
   )
